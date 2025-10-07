d821381e9ef42b29d919ab51bc1519733a5a81db
Praveenkumar <fistotech04@gmail.com>
2025-10-07 17:03:15 +0530

    Hash updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import {Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import UploadPage from './Components/UploadPage'
-import FlipbookPreview from './Components/FlipbookPreview'
+import { Route, HashRouter, Routes } from "react-router-dom";
+import UploadPage from "./Components/UploadPage";
+import FlipbookPreview from "./Components/FlipbookPreview";
 
 function App() {
   return (
     <>
-    <Router basename="/Flip_Book_Customize">
-      <Routes>
-        <Route path="/" element={<UploadPage />} />
-        <Route path="/flipbook" element={<FlipbookPreview />} />
-      </Routes>
-    </Router>
-    {/* <FlipbookSplit/> */}
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<UploadPage />} />
+          <Route path="/flipbook" element={<FlipbookPreview />} />
+        </Routes>
+      </HashRouter>
+      {/* <FlipbookSplit/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
